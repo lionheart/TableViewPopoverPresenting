f5372fddb0af55ec419d123237c8a3be8136ba19
Dan Loewenherz <dan@lionheartsw.com>
2016-06-19 22:10:19 -0500

    other changes

--- Example/TableViewPopoverPresenting_UITests/TableViewPopoverPresenting_UITests.swift
+++ Example/TableViewPopoverPresenting_UITests/TableViewPopoverPresenting_UITests.swift
@@ -7,30 +7,31 @@
 //
 
 import XCTest
+//import Nimble
+//import Quick
+@testable import TableViewPopoverPresenting
 
-class TableViewPopoverPresenting_UITests: XCTestCase {
-        
+class TableViewPopoverPresenting_ExampleUITests: XCTestCase {
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+
+        //        describe("the first table view row") {
+        //            it("displays a popover when you tap it") {
+        //                tablesQuery.staticTexts["Cell #0"].tap()
+        //                app.sheets["Important Question"].collectionViews.buttons["Red"].tap()
+        //            }
+        //        }
+
+        tablesQuery.staticTexts["Cell #1"].tap()
+        app.alerts["Hey"].collectionViews.buttons[":("].tap()
     }
-    
 }
