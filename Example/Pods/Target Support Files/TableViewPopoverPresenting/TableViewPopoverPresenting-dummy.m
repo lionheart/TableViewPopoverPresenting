b7aadf90894841c176831ef64372b4e2198ffee2
Dan Loewenherz <dan@lionheartsw.com>
2016-06-19 22:18:24 -0500

    update pod target support files

---
+++ Example/Pods/Target Support Files/TableViewPopoverPresenting/TableViewPopoverPresenting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TableViewPopoverPresenting : NSObject
+@end
+@implementation PodsDummy_TableViewPopoverPresenting
+@end
---
+++ Example/Pods/Target Support Files/TableViewPopoverPresenting/TableViewPopoverPresenting-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double TableViewPopoverPresentingVersionNumber;
+FOUNDATION_EXPORT const unsigned char TableViewPopoverPresentingVersionString[];
+
